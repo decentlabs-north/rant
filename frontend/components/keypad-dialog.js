eefb189685ec4e3943960d859840b3064a989070
Tony Ivanov <telamohn@pm.me>
2022-10-04 00:26:30 +0200

    Moved CSS into shadow boundary

--- frontend/components/keypad-dialog.js
+++ frontend/components/keypad-dialog.js
@@ -1,5 +1,5 @@
 import Tonic from '@socketsupply/tonic/index.esm.js'
-import './keypad-dialog.css'
+// import './keypad-dialog.css'
 import { write, next, get } from 'piconuro'
 import { nEl } from '../surgeon.js'
 import { decrypt } from '../encryption.js'
@@ -20,7 +20,7 @@ Tonic.add(class KeypadDialog extends Tonic {
         KeyPad.value = KeyPadVal + e.target.value
         break
       case 'lockBtn':
-        await psuhSecret(KeyPadVal)
+        await pushSecret(KeyPadVal)
         nEl('edit-keypad-dlg').open = false
         break
       case 'BackspaceBtn':
@@ -80,6 +80,83 @@ Tonic.add(class KeypadDialog extends Tonic {
       </div>
     `
   }
+
+  static stylesheet () {
+    return `
+      .keypad{
+        position: relative;
+        display: flex;
+        flex-direction: column;
+        padding: 1rem;
+        width: fit-content;
+      }
+      .keypad .num-button{
+        width: 100%;
+        margin: 0.1rem !important;
+        border-radius: 0;
+      }
+      .keypad .lock-button{
+        margin: 0.1rem !important;
+      }
+      .keypad .row{
+        justify-content: space-around;
+      }
+      .keypad .backspace-button{
+        width: auto !important;
+        margin: auto !important;
+        margin-bottom: 1rem !important;
+        border-top-left-radius: 0 !important;
+        border-bottom-left-radius: 0 !important;
+      }
+      #KeyPadDisplay{
+        border-top-right-radius: 0;
+        border-bottom-right-radius: 0;
+      }
+      #KeyPadDisplayUnlock{
+        border-top-right-radius: 0;
+        border-bottom-right-radius: 0;
+      }
+      #KeyPadDisplayUnlock::placeholder {
+        color: #f14646;
+        opacity: 1;
+      }
+      .keypad-article{
+        overflow: hidden;
+      }
+      .unlock-keypad-title{
+        display: flex;
+        transition: 0.2s;
+      }
+      .unlock-keypad-title span{
+        text-align: center;
+        margin: auto;
+        font-size: 1.5rem;
+      }
+
+      .close-unlock{
+        top: -100px;
+      }
+      .close-lock{
+        top: -60px;
+      }
+      .close-btn{
+        position: absolute;
+        width: fit-content;
+        background-color:rgba(1, 1, 1, 0);
+        border: none;
+        box-shadow: inset -3px -3px 15px rgba(0, 0, 0, 0.1);
+        border-bottom-right-radius: 1.8rem;
+        left: -30px;
+      }
+      .close-btn:hover{
+        background-color:#e75e5e;
+        box-shadow: 3px 3px 10px rgba(0, 0, 0, 0.3);
+      }
+      .close-btn:focus{
+        box-shadow: none;
+      }
+    `
+  }
 })
 
 function RemoveLastChar (value) {
@@ -87,7 +164,7 @@ function RemoveLastChar (value) {
   return value
 }
 
-async function psuhSecret (secret) {
+async function pushSecret (secret) {
   await kernel.setSecret(secret)
   await kernel.encryptMessage(secret)
 }
